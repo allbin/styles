2859e4fcacd650cbf76296cd15166a8313c72ec0
Joakim Ottosson <joakim.ottosson@allbinary.se>
2024-05-27 14:34:59 +0200

    Implementing ChadIconButtons for testing and useEffect for loading state

--- src/components/Button/GalleryButton.tsx
+++ src/components/Button/GalleryButton.tsx
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Button from './Button';
 import IconButton from './IconButton';
-import { IconHalfCookie } from '@allbin/icons';
+import { IconHalfCookie, IconTrash } from '@allbin/icons';
 
 // For testing
 import { ChadButton } from './ChadButton';
+import { ChadIconButton } from './ChadIconButton';
 
 const Btn: React.FC = () => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (isLoading) {
+      setTimeout(() => {
+        setIsLoading(false);
+      }, 3000);
+    }
+  }, [isLoading]);
+
   return (
     <div>
       <div className="mb-8 flex flex-wrap gap-2">
-        <ChadButton>Standard button</ChadButton>
+        <ChadButton onClick={() => console.log('click')}>
+          Standard button
+        </ChadButton>
         <ChadButton variant="filled" disabled>
           Filled button
         </ChadButton>
@@ -20,8 +33,8 @@ const Btn: React.FC = () => {
         </ChadButton>
       </div>
       <div className="mb-8 flex flex-wrap gap-2">
-        <ChadButton size="sm">Small default button</ChadButton>
-        <ChadButton size="md">Medium default button</ChadButton>
+        <ChadButton size="sm">Small button</ChadButton>
+        <ChadButton size="md">Default medium button</ChadButton>
         <ChadButton size="lg">Large button</ChadButton>
         <ChadButton size="xl">Extra large button</ChadButton>
       </div>
@@ -30,11 +43,42 @@ const Btn: React.FC = () => {
           Start icon button
         </ChadButton>
         <ChadButton endIcon={<IconHalfCookie />}>End icon button</ChadButton>
+        <ChadButton size="lg" endIcon={<IconHalfCookie />}>
+          Large end icon button
+        </ChadButton>
+        <ChadButton startIcon={<IconHalfCookie />} endIcon={<IconTrash />}>
+          Start and End icon button
+        </ChadButton>
       </div>
       <div className="mb-8 flex flex-wrap gap-2">
         <ChadButton loading={true} endIcon={<IconHalfCookie />}>
           Loading button
         </ChadButton>
+        <ChadButton
+          loading={isLoading}
+          startIcon={<IconHalfCookie />}
+          onClick={() => setIsLoading(true)}
+        >
+          Click for loading state
+        </ChadButton>
+      </div>
+      <div className="mb-8 flex flex-wrap gap-2">
+        <ChadIconButton icon={<IconHalfCookie />} />
+        <ChadIconButton variant="filled" icon={<IconHalfCookie />} />
+        <ChadIconButton variant="ghost" icon={<IconHalfCookie />} />
+        <ChadIconButton variant="outline" icon={<IconHalfCookie />} />
+        <ChadIconButton loading={true} icon={<IconHalfCookie />} />
+        <ChadIconButton disabled icon={<IconHalfCookie />} />
+        <ChadIconButton
+          icon={<IconTrash />}
+          onClick={() => console.log('Delete')}
+        />
+      </div>
+      <div className="mb-8 flex flex-wrap gap-2">
+        <ChadIconButton size="sm" icon={<IconHalfCookie />} />
+        <ChadIconButton size="md" icon={<IconHalfCookie />} />
+        <ChadIconButton size="lg" icon={<IconHalfCookie />} />
+        <ChadIconButton size="xl" icon={<IconHalfCookie />} />
       </div>
       <div className="mb-8 flex flex-wrap gap-2">
         <Button red>Red button</Button>
