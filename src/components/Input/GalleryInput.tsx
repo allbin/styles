095760338513f3db08d9739dc29b8e6ce505549b
Joakim Ottosson <joakim.ottosson@allbinary.se>
2024-05-30 12:45:33 +0200

    Added: 1. Number input 2. Tooltip 3. and exemple of a input field and button

--- src/components/Input/GalleryInput.tsx
+++ src/components/Input/GalleryInput.tsx
@@ -1,70 +1,87 @@
 import React from 'react';
 import Input from './Input';
 import { FaceSmileIcon, TrashIcon } from '@heroicons/react/24/solid';
+import Button from '../Button/Button';
 
 // For testing
 import { ShadInput } from './ShadInput';
 
 const GalleryInput: React.FC = () => {
-  const [value, setValue] = React.useState('');
+  const [textValue, setTextValue] = React.useState('');
+  const [numberValue, setNumberValue] = React.useState(100);
   return (
     <div>
       <h2 className="mb-4">Inputs</h2>
       <h3>Input fields</h3>
       <div className="mb-8 flex flex-col gap-2 rounded-md border border-gray-300 p-4">
-        <ShadInput min={10} max={100} id="shad-1" />
-        <ShadInput type="text" id="shad-1" />
-        <ShadInput type="text" id="shad-2" disabled />
-        <ShadInput type="text" id="shad-3" error="This is an error message" />
+        <ShadInput id="shad-1" />
+        <ShadInput id="shad-2" disabled />
+        <ShadInput id="shad-3" error="This is an error message" />
         <ShadInput
-          type="text"
           id="shad-3-1"
           placeholder="Type error to see error message"
-          value={value}
-          onChange={(e) => setValue(e.target.value)}
-          error={value === 'error' ? 'input error' : undefined}
+          value={textValue}
+          onChange={(e) => setTextValue(e.target.value)}
+          error={textValue === 'error' ? 'input error' : undefined}
         />
-        <ShadInput
-          type="text"
-          id="shad-4"
-          placeholder="This is a placeholder"
-        />
+        <ShadInput id="shad-4" placeholder="This is a placeholder" />
         <ShadInput
           id="shad-5"
-          type="text"
           startAdornment={<FaceSmileIcon className="size-5" />}
         />
         <ShadInput
           id="shad-6"
-          type="text"
           endAdornment={<FaceSmileIcon className="size-5" />}
         />
         <ShadInput
           id="shad-6"
-          type="text"
           startAdornment={<FaceSmileIcon className="size-5" />}
           endAdornment={<TrashIcon className="size-5" />}
         />
-        <ShadInput id="shad-7" type="email" />
+        <ShadInput
+          id="982"
+          type="number"
+          placeholder="Type error to see error message"
+          value={numberValue}
+          onChange={(e) => setNumberValue(Number(e.target.value))}
+        />
+        <ShadInput
+          id="13123"
+          toolTip="This is a tool tip"
+          placeholder="Hover to see tooltip"
+        />
       </div>
       <h3>Labels</h3>
-      <div className="mb-8 flex flex-col gap-2 rounded-md border border-gray-300 p-4">
-        <ShadInput type="text" id="shad-8" label="This is a label" />
+      <div className="mb-8 flex flex-col gap-2 rounded-md border border-gray-300 p-4 pt-10">
+        <ShadInput id="shad-8" label="This is a label with absolute position" />
       </div>
       <h3>Helper Text</h3>
-      <div className="mb-8 flex flex-col gap-2 rounded-md border border-gray-300 p-4">
-        <ShadInput type="text" id="shad-8" helperText="This is a helper text" />
+      <div className="mb-8 flex flex-col gap-2 rounded-md border border-gray-300 p-4 pb-10">
+        <ShadInput
+          id="shad-8"
+          helperText="This is a helper text with absolute position"
+        />
+      </div>
+      <h3>Input with button example</h3>
+      <div className="mb-8 flex flex-row gap-2 rounded-md border border-gray-300 p-4 py-10">
+        <ShadInput
+          id="shad-123"
+          placeholder="ex John Doe"
+          label="Name"
+          helperText="Enter your first and last name"
+        />
+        <Button variant="filled">Add</Button>
       </div>
       <Input id="input-1" />
       <Input id="input-2" label="This is a label" />
       <Input
         id="input-3"
-        value={value}
-        onChange={(e) => setValue(e.target.value)}
+        value={textValue}
+        onChange={(e) => setTextValue(e.target.value)}
         label="Input Label with placeholder and tooltip (invalid if value is 'invalid')"
         placeholder="Placeholder"
         tooltip="This is a tooltip"
-        invalid={value === 'invalid' ? 'Invalid input' : undefined}
+        invalid={textValue === 'invalid' ? 'Invalid input' : undefined}
       />
       <Input disabled id="input-3" label="Disabled Input" />
       <Input
