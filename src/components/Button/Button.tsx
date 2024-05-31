cbd9ea2bcf043e0983bb51bb584a56c795192da1
Joakim Ottosson <joakim.ottosson@allbinary.se>
2024-05-31 09:38:24 +0200

    - Removed icon size variables and using variants instead to set the icon size based on button size. - Added after: class to handle the hover, disabled, active in all of the variants.

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -7,38 +7,40 @@ import { Spinner } from '../spinner';
 const buttonVariants = cva(
   [
     'flex',
+    'relative',
     'font-medium',
     'items-center',
     'justify-center',
     'gap-2',
     'border',
     'transition-colors',
+    'rounded-md',
     'disabled:border-gray-400',
-    'disabled:bg-transparent',
-    'disabled:text-gray-400',
+    'disabled:bg-primary-300',
+    'disabled:text-primary-600',
     'disabled:active:opacity-100',
+    'disabled:pointer-events-none',
+    'active:opacity-80',
+    'after:content-[""]',
+    'after:absolute',
+    'after:top-0',
+    'after:left-0',
+    'after:w-full',
+    'after:h-full',
+    'after:rounded-md',
+    'after:bg-black',
+    'after:opacity-0',
+    'hover:after:opacity-10',
+    'active:after:opacity-30',
+    'disabled:after:opacity-50',
+    'disabled:after:mix-blend-difference',
   ],
   {
     variants: {
       variant: {
-        filled: [
-          'border-primary-500',
-          'bg-primary-500',
-          'text-text-50',
-          'hover:border-primary-500',
-          'hover:bg-primary-700',
-          'disabled:border-primary-300',
-          'disabled:bg-primary-300',
-          'disabled:text-text-700',
-          'disabled:hover:border-primary-300',
-          'disabled:hover:bg-primary-300',
-        ],
-        outline: [
-          'border-primary-600',
-          'hover:bg-primary-200',
-          'active:opacity-80',
-        ],
-        ghost: ['border-none', 'bg-transparent', 'hover:bg-primary-100'],
+        filled: ['border-primary-500', 'bg-primary-500', 'text-text-50'],
+        outline: ['border-primary-600', 'bg-transparent'],
+        ghost: ['border-none', 'bg-transparent'],
       },
       size: {
         sm: ['h-[24px]', 'text-sm'],
@@ -48,78 +50,94 @@ const buttonVariants = cva(
       },
       color: {
         base: [],
-        red: [],
-        green: [],
+        red: ['border-red-700', 'text-red-700'],
+        green: ['border-green-700', 'text-green-700'],
+      },
+      icon: {
+        true: ['aspect-square', 'rounded-full', 'after:rounded-full', 'p-0'],
+        false: ['px-5', 'py-1'],
+      },
+      round: {
+        true: ['rounded-full', 'after:rounded-full'],
+        false: [],
+      },
+      hasStartEndIcon: {
+        true: [],
+        false: [],
       },
     },
     compoundVariants: [
       {
         color: 'red',
         variant: 'outline',
-        class: [
-          'border-red-700',
-          'text-red-700',
-          'hover:bg-red-200',
-          'hover:text-red-600',
-        ],
+        class: ['border-red-700', 'text-red-700'],
       },
       {
         color: 'red',
         variant: 'filled',
-        class: [
-          'bg-red-700',
-          'text-text-50',
-          'hover:border-red-800',
-          'hover:bg-red-600',
-          'hover:text-red-50',
-          'disabled:border-red-900/80',
-          'disabled:bg-red-900/60',
-          'disabled:text-red-900',
-          'disabled:hover:border-red-900/80',
-          'disabled:hover:bg-red-900/60',
-        ],
+        class: ['bg-red-700', 'text-text-50'],
       },
       {
         color: 'green',
         variant: 'outline',
-        class: [
-          'border-green-700',
-          'text-green-700',
-          'hover:bg-green-200',
-          'hover:text-green-600',
-        ],
+        class: ['border-green-700', 'text-green-700'],
       },
       {
         color: 'green',
         variant: 'filled',
-        class: [
-          'bg-green-700',
-          'text-text-50',
-          'hover:border-green-800',
-          'hover:bg-green-600',
-          'hover:text-green-50',
-          'disabled:border-green-900/80',
-          'disabled:bg-green-900/60',
-          'disabled:text-green-900',
-          'disabled:hover:border-green-900/80',
-          'disabled:hover:bg-green-900/60',
-        ],
+        class: ['bg-green-700', 'text-text-50'],
+      },
+      {
+        size: 'sm',
+        hasStartEndIcon: true,
+        class: ['[&>*:first-child]:size-3'],
+      },
+      {
+        size: 'md',
+        hasStartEndIcon: true,
+        class: ['[&>*:first-child]:size-5'],
+      },
+      {
+        size: 'lg',
+        hasStartEndIcon: true,
+        class: ['[&>*:first-child]:size-6'],
+      },
+      {
+        size: 'xl',
+        hasStartEndIcon: true,
+        class: ['[&>*:first-child]:size-7'],
+      },
+      {
+        size: 'sm',
+        icon: true,
+        class: ['[&>*:first-child]:size-3'],
+      },
+      {
+        size: 'md',
+        icon: true,
+        class: ['[&>*:first-child]:size-5'],
+      },
+      {
+        size: 'lg',
+        icon: true,
+        class: ['[&>*:first-child]:size-6'],
+      },
+      {
+        size: 'xl',
+        icon: true,
+        class: ['[&>*:first-child]:size-8'],
       },
     ],
     defaultVariants: {
       variant: 'outline',
       size: 'md',
       color: 'base',
+      round: false,
+      icon: false,
+      hasStartEndIcon: false,
     },
   },
 );
-
-export const iconSizes = {
-  sm: 'size-3',
-  md: 'size-5',
-  lg: 'size-6',
-  xl: 'size-7',
-};
 
 interface ButtonProps
   extends Omit<React.ButtonHTMLAttributes<HTMLButtonElement>, 'color'>,
@@ -138,45 +156,43 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
     {
       className,
       variant,
-      size,
+      size = 'md',
       color,
-      icon,
+      icon = false,
       startIcon,
       endIcon,
       type = 'button',
       loading = false,
-      round,
+      round = false,
       asChild = false,
       ...props
     },
     ref,
   ) => {
     const Comp = asChild ? Slot : 'button';
-
-    const buttonSize = (size && iconSizes[size]) || iconSizes.md;
-
-    const roundClasses = round ? 'rounded-full' : 'rounded-md';
-    const iconClasses = icon
-      ? ['aspect-square', 'rounded-full', 'p-0']
-      : ['px-5', 'py-1'];
+    const hasStartEndIcon = !!startIcon || !!endIcon;
 
     return (
       <Comp
         className={cn(
-          buttonVariants({ variant, size, color, className }),
-          roundClasses,
-          iconClasses,
+          buttonVariants({
+            variant,
+            size,
+            color,
+            className,
+            icon,
+            round,
+            hasStartEndIcon,
+          }),
         )}
         ref={ref}
         type={type}
         {...props}
       >
-        {loading && <Spinner className={buttonSize} />}
-        {!loading && startIcon && (
-          <span className={buttonSize}>{startIcon}</span>
-        )}
+        {loading && <Spinner />}
+        {!loading && startIcon && <span>{startIcon}</span>}
         {props.children}
-        {!loading && endIcon && <span className={buttonSize}>{endIcon}</span>}
+        {!loading && endIcon && <span>{endIcon}</span>}
       </Comp>
     );
   },
