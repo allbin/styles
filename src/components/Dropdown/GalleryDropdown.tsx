6d71515d5689c82361f88ea7e961ff089cf70ca3
Joakim Ottosson <joakim.ottosson@allbinary.se>
2024-06-14 10:54:14 +0200

    Added data with no categories and changed to different variants.

--- src/components/Dropdown/GalleryDropdown.tsx
+++ src/components/Dropdown/GalleryDropdown.tsx
@@ -3,13 +3,20 @@ import Button from '../Button/Button';
 import { Dropdown, OptionsType, OptionsProps } from './Dropdown';
 
 const GalleryDropdown: React.FC = () => {
-  const dropdownData: OptionsProps[] = [
+  const dropdownCategoryData: OptionsProps[] = [
     { id: '1', label: 'Apple' },
     { id: '2', label: 'Orange', color: 'green' },
     { category: 'Drinks' },
     { id: '4', label: 'Cola' },
     { id: '5', label: 'Fanta' },
     { category: 'Admin' },
+    { id: '7', label: 'Remove all items', color: 'red' },
+  ];
+  const dropdownData: OptionsProps[] = [
+    { id: '1', label: 'Apple' },
+    { id: '2', label: 'Orange', color: 'green' },
+    { id: '4', label: 'Cola' },
+    { id: '5', label: 'Fanta' },
     { id: '7', label: 'Remove all items', color: 'red' },
   ];
 
@@ -21,11 +28,10 @@ const GalleryDropdown: React.FC = () => {
   return (
     <div>
       <h2 className="mb-4">Dropdown</h2>
-      <h3>Dropdown</h3>
+      <h3>Variants</h3>
       <div className="mb-8 flex flex-col gap-2 rounded-md border border-gray-300 p-4">
         <Dropdown
           id="dropdown-1"
-          className="w-[320px]"
           placeholder="Default dropdown with onChange"
           onChange={(value) => {
             console.log('From list: ', value);
@@ -34,7 +40,6 @@ const GalleryDropdown: React.FC = () => {
         />
         <Dropdown
           id="dropdown-2"
-          className="w-[320px]"
           placeholder="This one is disabled"
           onChange={(value) => {
             console.log('From list: ', value);
@@ -44,7 +49,6 @@ const GalleryDropdown: React.FC = () => {
         />
         <Dropdown
           id="dropdown-3"
-          className="w-[320px]"
           placeholder="Selected value"
           value={selectedValue}
           onChange={(value) => {
@@ -53,18 +57,33 @@ const GalleryDropdown: React.FC = () => {
           options={dropdownData}
         />
         <Dropdown
-          id="dropdown-3"
-          className="w-[320px]"
+          id="dropdown-4"
           placeholder="Error"
           error
           errorMessage="This is an error"
           options={dropdownData}
         />
+        <Dropdown
+          id="dropdown-5"
+          placeholder="300px width"
+          dropDownWidth={'300px'}
+          options={dropdownData}
+        />
+        <Dropdown
+          id="dropdown-6"
+          placeholder="With categories"
+          options={dropdownCategoryData}
+        />
+        <Dropdown
+          id="dropdown-7"
+          placeholder="Defined hight on option box"
+          optionsContainerHeight={'200px'}
+          options={dropdownCategoryData}
+        />
       </div>
       <div className="mb-8 flex flex-col gap-2 rounded-md border border-gray-300 p-4 pt-8">
         <Dropdown
-          id="dropdown-4"
-          className="w-[320px]"
+          id="dropdown-8"
           placeholder="With label"
           label="This is a label with absolute position"
           options={dropdownData}
@@ -72,26 +91,16 @@ const GalleryDropdown: React.FC = () => {
       </div>
       <div className="mb-8 flex flex-col gap-2 rounded-md border border-gray-300 p-4 pb-8">
         <Dropdown
-          id="dropdown-5"
-          className="w-[320px]"
+          id="dropdown-9"
           placeholder="With helper text"
           helperText="This is a helper text with absolute position"
           options={dropdownData}
         />
       </div>
-      <div className="mb-8 flex flex-col gap-2 rounded-md border border-gray-300 p-4 pb-8">
+      <h3>Dropdown exemple</h3>
+      <div className="mb-8 flex w-full flex-row gap-2 rounded-md border border-gray-300 p-4 py-10">
         <Dropdown
-          id="dropdown-7"
-          className="w-[320px]"
-          placeholder="Defined hight on option box"
-          optionsContainerHeight={'200px'}
-          options={dropdownData}
-        />
-      </div>
-      <div className="mb-8 flex flex-row gap-2 rounded-md border border-gray-300 p-4 py-10">
-        <Dropdown
-          id="dropdown-6"
-          className="w-[320px]"
+          id="dropdown-10"
           placeholder="Choose an option"
           label="Exemple with button"
           helperText="This is a helper text with absolute position"
@@ -100,7 +109,9 @@ const GalleryDropdown: React.FC = () => {
             console.log('Selected: ', value);
           }}
         />
-        <Button variant="filled">Next step</Button>
+        <Button className="w-[165px]" variant="filled">
+          Next step
+        </Button>
       </div>
     </div>
   );
