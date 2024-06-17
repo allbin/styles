51ea16a744f1d0546d7d8a26824d38f1f1413b7d
Joakim Ottosson <joakim.ottosson@allbinary.se>
2024-06-17 07:43:52 +0200

    Changed some paddings and added scheme level information.

--- src/components/LeftPanel/LeftPanel.tsx
+++ src/components/LeftPanel/LeftPanel.tsx
@@ -4,8 +4,9 @@ import { HomeIcon } from '@heroicons/react/24/outline';
 
 const LeftPanel: React.FC = () => {
   return (
-    <div className="w-full">
-      <div className="flex flex-col gap-1">
+    <div className="size-full bg-background-200">
+      <div className="flex flex-col gap-1 p-4">
+        <span className="mb-3 text-sm font-bold">Level - 200</span>
         <NavElement to="/">
           <HomeIcon className="size-5" />
           Start
